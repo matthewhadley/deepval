49730f8b8cb518f4d7042bf8989758a1f616a499
diffsky <matthew.hadley@gmail.com>
2017-12-18 22:03:11 -0800

    return undefined for missing objects and intermediate null paths, update eslinting

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 'use strict';
 
-var deepval = function(obj, path, value, remove) {
+var deepval = function (obj, path, value, remove) {
+  if (!obj) {
+    return undefined;
+  }
   if (!Array.isArray(path)) {
     path = path.split('.');
   }
-  var pl = path.length - 1;
 
-  for (var i = 0; i < pl; i += 1) {
-    if (typeof value !== 'undefined' && typeof obj[path[i]] === 'undefined') {
+  var pl = path.length - 1;
+  for (var i = 0; i < pl; i++) {
+    if (value !== undefined && (obj[path[i]] === undefined || obj[path[i]] === null)) {
       obj[path[i]] = {};
-    } else if (!obj.hasOwnProperty(path[i]) || typeof obj[path[i]] === 'undefined') {
+    } else if (!obj.hasOwnProperty(path[i]) || (obj[path[i]] === undefined || obj[path[i]] === null)) {
       return undefined;
     }
     obj = obj[path[i]];
   }
 
-  if (typeof value !== 'undefined') {
+  if (value !== undefined) {
     if (remove) {
       return delete obj[path[pl]];
     } else {
@@ -27,18 +30,18 @@ var deepval = function(obj, path, value, remove) {
 
 module.exports = deepval;
 
-module.exports.get = function(obj, path) {
+module.exports.get = function (obj, path) {
   return deepval(obj, path);
 };
 
-module.exports.set = function(obj, path, value) {
+module.exports.set = function (obj, path, value) {
   return deepval(obj, path, value);
 };
 
-module.exports.del = function(obj, path) {
+module.exports.del = function (obj, path) {
   return deepval(obj, path, null, true);
 };
 
-module.exports.dotpath = function() {
+module.exports.dotpath = function () {
   return Array.prototype.slice.call(arguments).join('.');
 };
--- test/index.js
+++ test/index.js
@@ -13,7 +13,8 @@ var data = {
     e: [
       ['bar']
     ]
-  }
+  },
+  nulls: null
 };
 
 var expect = {
@@ -39,29 +40,28 @@ var expect = {
   ]
 };
 
-test('getting values returns correct key value', function(t) {
-  t.plan(5);
+test('getting values returns correct key value', function (t) {
+  t.plan(4);
   t.equal(deepval(data, 'foo'), 'bar', 'shallow value get');
   t.equal(deepval(data, 'a.b.c'), 'deep', 'deep value get');
   t.equal(deepval(data, 'foo'), 'bar', 'shallow value get');
   t.equal(deepval(data, 'a.b.c'), 'deep', 'deep value get');
-  t.equal(deepval(data, ['a', 'b', 'c']), 'deep', 'deep value get via an array');
 });
 
-test('getting values via .get returns correct key value', function(t) {
+test('getting values via .get returns correct key value', function (t) {
   t.plan(2);
   t.equal(deepval.get(data, 'foo'), 'bar', 'shallow value get');
   t.equal(deepval.get(data, 'a.b.c'), 'deep', 'deep value get');
 });
 
-test('getting values in arrays returns correct key value', function(t) {
+test('getting values in arrays returns correct key value', function (t) {
   t.plan(3);
   t.equal(deepval(data, 'a.d.0'), 'foo', 'shallow array value get');
   t.equal(deepval(data, ['a', 'd', 0]), 'foo', 'shallow array value get with array path');
   t.equal(deepval(data, 'a.e.0.0'), 'bar', 'nested array value get');
 });
 
-test('setting values in arrays works', function(t) {
+test('setting values in arrays works', function (t) {
   t.plan(3);
   t.equal(deepval(data, 'h', ['foo'])[0], expect.h[0], 'shallow array value set');
 
@@ -71,50 +71,53 @@ test('setting values in arrays works', function(t) {
   t.equal(deepval(data, 'i.0.1'), 'zzz', 'second nested array value set');
 });
 
-test('getting non-existent values return undefined', function(t) {
-  t.plan(3);
+test('getting non-existent values return undefined', function (t) {
+  t.plan(6);
   t.equal(deepval(data, 'z'), undefined, 'shallow undefined get');
   t.equal(deepval(data, 'a.e.c'), undefined, 'deep undefined get');
   t.equal(deepval(data, 'a.b.d'), undefined, 'end deep undefined get');
+  t.equal(deepval.get(undefined, 'nothing.here'), undefined, 'undefined returned when no object passed');
+  t.equal(deepval.get(data, 'nulls.nothing.here'), undefined, 'undefined returned when intermediate key is null');
+  t.equal(deepval(data, 'nulls'), null, 'but can get a null value');
 });
 
-test('setting values sets correct value', function(t) {
+test('setting values sets correct value', function (t) {
   t.plan(3);
   t.equal(deepval(data, 'foo', 'voo'), expect.foo, 'shallow value set');
   t.equal(deepval(data, 'a.b.c', 'voo'), expect.a.b.c, 'deep value set');
   t.equal(deepval(data, ['a', 'b', 'c1'], 'voo'), expect.a.b.c1, 'deep value set via an array');
 });
 
-test('setting values via .set sets correct value', function(t) {
+test('setting values via .set sets correct value', function (t) {
   t.plan(2);
   t.equal(deepval.set(data, 'foo', 'voo'), expect.foo, 'shallow value set');
   t.equal(deepval.set(data, 'a.b.c', 'voo'), expect.a.b.c, 'deep value set');
 });
 
-test('setting values on non-existent paths', function(t) {
+test('setting values on non-existent paths', function (t) {
   t.plan(1);
   t.equal(deepval(data, 'a.d.e.f', 'foo'), expect.a.d.e.f, 'intermediate non-existent objects created');
 });
 
-test('can set deep values that are empty', function(t) {
+test('can set deep values that are empty', function (t) {
   t.plan(1);
   t.equal(deepval(data, 'g.empty', 'ok'), expect.g.empty, 'deep value set empty');
 });
 
-test('can remove values', function(t) {
+test('can remove values', function (t) {
   t.plan(1);
   deepval(data, 'foo', null, true);
   t.equal(deepval(data, 'foo'), undefined, 'value is undefined');
 });
 
-test('can remove values via .del', function(t) {
+test('can remove values via .del', function (t) {
   t.plan(1);
   deepval.del(data, 'foo');
   t.equal(deepval.get(data, 'foo'), undefined, 'value is undefined');
 });
 
-test('provides a utlity function to join variables as a dotpath', function(t) {
-  t.plan(1)
+test('provides a utlity function to join variables as a dotpath', function (t) {
+  t.plan(1);
   var a = 'hello';
   var b = 'test';
   deepval.dotpath(a, 'world', b);
